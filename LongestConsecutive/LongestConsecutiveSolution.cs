963f11720c9c0687b249988f5ed4b3c59494b0ac
Naveed Ahmed Khan <nksherani@outlook.com>
2022-04-12 18:03:39 -0400

    Add project files.

---
+++ LongestConsecutive/LongestConsecutiveSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestConsecutive
+{
+    public class LongestConsecutiveSolution
+    {
+        public static int Execute(List<int> numbers)
+        {
+            int max = 0;
+            List<int> sorted = numbers.Distinct().OrderBy(x => x).ToList();
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                int localMax = 0;
+                bool broken = false;
+                for (int j = i; j < sorted.Count - 1; j++)
+                {
+                    if(sorted[j+1] - sorted[j] == 1)
+                    {
+                        localMax++;
+                    }
+                    else
+                    {
+                        localMax++;
+                        broken = true;
+                        break;
+                    }
+                }
+                if(!broken)
+                    localMax++;
+                if(localMax > max)
+                    max = localMax;
+            }
+            return max;
+        }
+    }
+}
---
+++ LongestConsecutive/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LongestConsecutive;
+
+Console.WriteLine("Hello, World!");
+List<int> list = new List<int>() { 1, 2, 3 };
+Console.WriteLine(LongestConsecutiveSolution.Execute(list));
+
+list = new List<int>() { 6, 7, 3, 1, 100, 102, 6, 12 };
+Console.WriteLine(LongestConsecutiveSolution.Execute(list));
+
+list = new List<int>() { 5, 6, 1, 2, 8, 9, 7 };
+Console.WriteLine(LongestConsecutiveSolution.Execute(list));
+
+list = new List<int>() { 5, 8, 10, 11 };
+Console.WriteLine(LongestConsecutiveSolution.Execute(list));
+
+list = new List<int>() { 5 };
+Console.WriteLine(LongestConsecutiveSolution.Execute(list));
